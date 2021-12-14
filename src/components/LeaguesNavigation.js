eb1feb5482a89948df90153d6855f54db42edfa5
dctowers <dctorres@uc.cl>
2021-12-14 18:29:17 -0300

    cambios a squad y css

--- src/components/LeaguesNavigation.js
+++ src/components/LeaguesNavigation.js
@@ -3,13 +3,13 @@ import { Context } from '../store/AppContext'
 import { Link } from 'react-router-dom'
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import './LeaguesNavigation.css';
+import '../views/LeaguesNavigation.css';
 
 function LeaguesNavigation(props) {
     console.log(props)
     const { store, actions } = useContext(Context)
-    //const [ squad, setSquad ] = useState(store.l1Squad)   //this is the real deal
-    //const [ squad, setSquad ] = useState(actions.getHardCodedSquad())   // this is only for developing whithout backend
+    // const [ squad, setSquad ] = useState(store.l1Squad)   //this is the real deal
+    const [ squad, setSquad ] = useState(actions.getHardCodedSquad())   // this is only for developing whithout backend
     //console.log('squad.lst :', squad.lst)
 
     const list = document.querySelectorAll('.list');
@@ -28,31 +28,31 @@ function LeaguesNavigation(props) {
                     <li className="list active">
                         <button onClick={() => props.onClick(store.plSquad)}>
                             <span className="text">Premier League</span>
-                            <span className="icon" ><AiIcons.AiFillHome /></span>
+                            <span className="league-img" ><AiIcons.AiFillHome /></span>
                         </button>
                     </li>
                     <li className="list">
                         <button onClick={() => props.onClick(store.l1Squad)}>
                             <span className="text">Ligue 1</span>
-                            <span className="icon" ><AiIcons.AiFillHome /></span>
+                            <span className="league-img" ><AiIcons.AiFillHome /></span>
                         </button>
                     </li>
                     <li className="list">
                         <button onClick={() => props.onClick(store.blSquad)}>
                             <span className="text">Bundesliga</span>
-                            <span className="icon"><AiIcons.AiFillHome /></span>
+                            <span className="league-img"><AiIcons.AiFillHome /></span>
                         </button>
                     </li>
                     <li className="list">
                         <button onClick={() => props.onClick(store.saSquad)}>
                             <span className="text">Serie A</span>
-                            <span className="icon" ><AiIcons.AiFillHome /></span>
+                            <span className="league-img" ><AiIcons.AiFillHome /></span>
                         </button>
                     </li>
                     <li className="list">
                         <button onClick={() => props.onClick(store.llSquad)}>
                             <span className="text">LaLiga</span>
-                            <span className="icon" ><AiIcons.AiFillHome /></span>
+                            <span className="league-img" ><AiIcons.AiFillHome /></span>
                         </button>
                     </li>
                     <div className="indicator"></div>
@@ -61,7 +61,5 @@ function LeaguesNavigation(props) {
         </div>
     )
 }
-
-
 
 export default LeaguesNavigation;
